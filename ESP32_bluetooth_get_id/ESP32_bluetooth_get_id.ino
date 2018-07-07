960cf6043cb907f09dc0fc3a828666b619c8fe0a
Matthew <bmatthwtaylor@gmail.com>
2018-07-07 13:30:26 +1000

    get bluetooth id, esp_bt_dev_set_device_name

--- ESP32_bluetooth_get_id/ESP32_bluetooth_get_id.ino
+++ ESP32_bluetooth_get_id/ESP32_bluetooth_get_id.ino
@@ -1,12 +1,15 @@
 /*
  * based on https://techtutorialsx.com/2018/03/09/esp32-arduino-getting-the-bluetooth-device-address/
  * example output : 30:AE:A4:21:D2:AA
+ * OUI Prefix     Company
+ * 30:AE:A4       Espressif Inc.
  */
 
 #include "esp_bt_main.h"
+#include "esp_gap_bt_api.h"
 #include "esp_bt_device.h"
  
-bool initBluetooth()
+bool initBluetooth(const char *deviceName)
 {
   Serial.println("initBluetooth():start");
   if (!btStart()) {
@@ -23,7 +26,10 @@ bool initBluetooth()
     Serial.println("Failed to enable bluedroid");
     return false;
   }
+  esp_bt_dev_set_device_name(deviceName); 
+  esp_bt_gap_set_scan_mode(ESP_BT_SCAN_MODE_CONNECTABLE_DISCOVERABLE);
   Serial.println("initBluetooth():end");
+  
 }
  
 void printDeviceAddress() {
@@ -48,9 +54,13 @@ void printDeviceAddress() {
 void setup() {
   Serial.begin(115200);
   Serial.println("ESP32_bluetooth_get_id setup:start");
-  initBluetooth();
+  if(!initBluetooth("ESP32 BT")){
+      Serial.println("Bluetooth init failed");
+  };
   printDeviceAddress();
   Serial.println("ESP32_bluetooth_get_id setup:end");
 }
  
-void loop() {}
+void loop() {
+  
+  }
